3e19b9e499aeaca67ae1eaab299d51e5f29f6635
Albert Rodriguez Franco <albertrfranco@gmail.com>
2020-09-24 16:01:26 +0200

    Loading texture names

--- ResourceCompiler/src/main.cpp
+++ ResourceCompiler/src/main.cpp
@@ -13,7 +13,41 @@ struct Mesh
 {
 	std::vector<Vertex> vertices;
 	std::vector<uint32_t> indices;
+	unsigned int materialIndex;
 };
+
+std::vector<std::string> LoadMaterials(const aiScene* scene)
+{
+	// Create 1:1 sized list of textures
+	std::vector<std::string> textureList(scene->mNumMaterials);
+
+	// Go through each material and copy its texture file name (if it exists)
+	for (size_t i = 0; i < scene->mNumMaterials; i++)
+	{
+		// Get the material
+		aiMaterial* material = scene->mMaterials[i];
+
+		// Initialize the texture to empty string (will be replaced if texture exists)
+		textureList[i] = "";
+
+		// Check for a Diffuse Texture (standard details texture)
+		if (material->GetTextureCount(aiTextureType_DIFFUSE))
+		{
+			// Get path of the texture file
+			aiString path;
+			if (material->GetTexture(aiTextureType_DIFFUSE, 0, &path) == AI_SUCCESS)
+			{
+				// Cut off any directory information already present
+				int idx = std::string(path.data).rfind("\\");
+				std::string fileName = std::string(path.data).substr(idx + 1);
+
+				textureList[i] = fileName;
+			}
+		}
+	}
+
+	return textureList;
+}
 
 Mesh LoadMesh(const aiMesh* mesh, const aiScene* scene)//, std::vector<int> matToTex)
 {
@@ -57,15 +91,13 @@ Mesh LoadMesh(const aiMesh* mesh, const aiScene* scene)//, std::vector<int> matT
 	}
 
 	// Create new mesh with details and return it
-	Mesh newMesh = { vertices, indices }; //, matToTex[mesh->mMaterialIndex]);
+	Mesh newMesh = { vertices, indices, mesh->mMaterialIndex }; //, matToTex[mesh->mMaterialIndex]);
 
 	return newMesh;
 }
 
-std::vector<Mesh> LoadNode(aiNode* node, const aiScene* scene)
+void LoadNode(aiNode* node, const aiScene* scene, std::vector<Mesh>& meshList)
 {
-	std::vector<Mesh> meshList;
-
 	// Go through each mesh at this node and create it, then add it to our meshList
 	for (size_t i = 0; i < node->mNumMeshes; i++)
 	{
@@ -77,24 +109,109 @@ std::vector<Mesh> LoadNode(aiNode* node, const aiScene* scene)
 	// Go through each node attached to this node and load it, then append their meshes to this node's mesh list
 	for (size_t i = 0; i < node->mNumChildren; i++)
 	{
-		std::vector<Mesh> newList = LoadNode(node->mChildren[i], scene); //, matToTex);
-		meshList.insert(meshList.end(), newList.begin(), newList.end());
+		LoadNode(node->mChildren[i], scene, meshList); //, matToTex);
 	}
-
-	return meshList;
 }
 
-void saveToBinary(const std::string& outputFile, const std::vector<Mesh>& meshList)
+void saveToBinary(const std::string& modelFile, const std::string& outputFile, std::vector<Mesh>& meshList)
 {
+	//Import model "scene"
+	Assimp::Importer importer;
+	const aiScene* scene = importer.ReadFile(modelFile, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices);
+	if (!scene)
+	{
+		throw std::runtime_error("Failed to load model! (" + modelFile + ")");
+	}
+
+	LoadNode(scene->mRootNode, scene, meshList);
+	
 	std::ofstream file(outputFile, std::ios::out | std::ios::binary);
 
 	if (!file.is_open())
 		throw std::runtime_error("Could not open file " + outputFile + " for writing!");
+	
+	size_t meshSize = meshList.size();
+	file.write(reinterpret_cast<const char*>(&meshSize), sizeof(size_t));
 
+	size_t numVertices;
+	size_t numIndices;
 	for (auto& mesh : meshList)
 	{
-		file.write((char*)&mesh.vertices, mesh.vertices.size() * sizeof(Vertex));
-		file.write((char*)&mesh.indices, mesh.indices.size() * sizeof(uint32_t));
+		numVertices = mesh.vertices.size();
+		file.write(reinterpret_cast<const char*>(&numVertices), sizeof(size_t));
+		file.write(reinterpret_cast<const char*>(&mesh.vertices[0]), numVertices * sizeof(Vertex));
+
+		numIndices = mesh.indices.size();
+		file.write(reinterpret_cast<const char*>(&numIndices), sizeof(size_t));
+		file.write(reinterpret_cast<const char*>(&mesh.indices[0]), numIndices * sizeof(uint32_t));
+
+		file.write(reinterpret_cast<const char*>(&mesh.materialIndex), sizeof(unsigned int));
+	}
+
+	std::vector<std::string> materials = LoadMaterials(scene);
+	size_t materialsSize = materials.size();
+	file.write(reinterpret_cast<const char*>(&materialsSize), sizeof(size_t));
+	for (auto& material : materials)
+	{
+		file.write(material.c_str(), material.size());
+		file.write("\0", sizeof(char));
+	}
+
+	file.close();
+}
+
+void loadFromBinary(const std::string& inputFile, std::vector<Mesh>& meshList, std::vector<std::string>& materials)
+{
+	std::ifstream file(inputFile, std::ios::in | std::ios::binary);
+
+	if (!file.is_open())
+		throw std::runtime_error("Could not open file " + inputFile + " for reading!");
+	
+	// Read how many meshes we have
+	size_t meshSize;
+	file.seekg(0, std::ios::beg);
+	file.read((char*)&meshSize, sizeof(size_t));
+
+	size_t vertexSize;
+	size_t indexSize;
+	Vertex v;
+	uint32_t index;
+	unsigned int materialIndex;
+
+	for (size_t i = 0; i < meshSize; i++)
+	{
+		file.read((char*)&vertexSize, sizeof(size_t));
+		std::vector<Vertex> vertices;
+		vertices.reserve(vertexSize);
+
+		for (size_t j = 0; j < vertexSize; j++)
+		{
+			file.read((char*)&v, sizeof(Vertex));
+			vertices.push_back(v);
+		}
+
+		file.read((char*)&indexSize, sizeof(size_t));
+		std::vector<uint32_t> indices;
+		indices.reserve(indexSize);
+		
+		for (size_t j = 0; j < indexSize; j++)
+		{
+			file.read((char*)&index, sizeof(uint32_t));
+			indices.push_back(index);
+		}
+
+		file.read((char*)&materialIndex, sizeof(unsigned int));
+
+		meshList.push_back(Mesh{ vertices, indices, materialIndex });
+	}
+
+	size_t materialsSize;
+	file.read((char*)&materialsSize, sizeof(size_t));
+	std::string material;
+	for (size_t i = 0; i < materialsSize; i++)
+	{
+		std::getline(file, material, '\0');
+		materials.push_back(material);
 	}
 
 	file.close();
@@ -105,17 +222,13 @@ int main()
 	const std::string modelFile = "models/uh60.obj";
 	const std::string outputFile = "uh60.bin";
 
-	// Import model "scene"
-	Assimp::Importer importer;
-	const aiScene* scene = importer.ReadFile(modelFile, aiProcess_Triangulate | aiProcess_FlipUVs | aiProcess_JoinIdenticalVertices);
-	if (!scene)
-	{
-		throw std::runtime_error("Failed to load model! (" + modelFile + ")");
-	}
-
 	// TODO: Load materials
-	std::vector<Mesh> meshList = LoadNode(scene->mRootNode, scene);
-	saveToBinary(outputFile, meshList);
+	std::vector<Mesh> writeList;
+	saveToBinary(modelFile, outputFile, writeList);
+
+	std::vector<Mesh> readList;
+	std::vector<std::string> materials;
+	loadFromBinary(outputFile, readList, materials);
 
 	std::cout << "Compiling Complete!" << std::endl;
 }
