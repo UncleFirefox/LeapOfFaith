29150ba11f86060a232c7457e0dd9f7b81042eaa
Albert Rodriguez <albert.rodriguez@seddi.com>
2021-12-28 13:48:31 +0100

    Adding shader compilation step

--- LeapOfFaithLib/src/shaders/compile_shaders.bat
+++
@@ -1,5 +0,0 @@
-::TODO: Make compilerPath agnostic (probably putting the sdk tools in source control?) 
-set compilerPath=C:\VulkanSDK\1.2.141.2\Bin32
-%compilerPath%\glslangValidator.exe -V shader.vert
-%compilerPath%\glslangValidator.exe -V shader.frag
-pause
